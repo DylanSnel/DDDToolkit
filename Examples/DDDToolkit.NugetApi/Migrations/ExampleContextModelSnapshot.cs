d6d56c0aedb657ac3644c3abd73d3080b3e895bf
Dylan Snel <dylan@diditsolutions.nl>
2024-04-11 21:29:23 +0200

    Final testing

--- Examples/DDDToolkit.NugetApi/Migrations/ExampleContextModelSnapshot.cs
+++ Examples/DDDToolkit.NugetApi/Migrations/ExampleContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace DDDToolkit.NugetApi.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.1")
+                .HasAnnotation("ProductVersion", "8.0.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
