bcdeead49b9662741db8e12ea47f69da58bd5206
Dylan Snel <dylan@diditsolutions.nl>
2024-09-10 09:02:17 +0200

    Added HotChocolate GraphQL Support

---
+++ Source/DDDToolkit.HotChocolate/Interceptors/IgnoreInternalFieldsInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using DDDToolkit.Abstractions.Attributes;
+using HotChocolate.Configuration;
+using HotChocolate.Types.Descriptors.Definitions;
+using System.Reflection;
+
+namespace DDDToolkit.HotChocolate.Interceptors;
+
+/// <summary>
+/// Ignores all fields with the InternalAttribute
+/// </summary>
+public class IgnoreInternalFieldsInterceptor : TypeInterceptor
+{
+    public override void OnBeforeCompleteType(
+        ITypeCompletionContext completionContext,
+        DefinitionBase definition)
+    {
+        if (definition is ObjectTypeDefinition objectTypeDef)
+        {
+            foreach (var field in objectTypeDef.Fields)
+            {
+                var memberInfo = field.Member;
+                if (memberInfo != null && memberInfo.GetCustomAttribute<InternalAttribute>() != null)
+                {
+                    field.Ignore = true;
+                }
+            }
+        }
+    }
+}
